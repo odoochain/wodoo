ae5a3579e59d67ee7c6479f400e1ca0c4328f43f
odoochain <75811889+odoochain@users.noreply.github.com>
2023-10-22 02:04:25 +0800

    fix to odoochain

--- wodoo/lib_src.py
+++ wodoo/lib_src.py
@@ -266,7 +266,7 @@ def setup_venv(config):
     click.secho("source '{}'".format(venv_dir / "bin" / "activate"))
     click.secho("pip3 install cython")
     click.secho(
-        "pip3 install -r https://gitee.com/paddlelaw/paddlelaw-templates/raw/{}/requirements.txt".format(current_version()))
+        "pip3 install -r https://gitee.com/odoochain/paddlelaw-templates/raw/{}/requirements.txt".format(current_version()))
     # "pip3 install -r https://raw.githubusercontent.com/odoo/odoo/{}/requirements.txt".format(current_version()))
     requirements1 = Path(__file__).parent.parent / 'images' / 'odoo' / 'config' / str(
         current_version()) / 'requirements.txt'
